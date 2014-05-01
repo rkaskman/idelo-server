72cb1afed9d81325319aafeee50016fda9246a5c
ilja <ilja.guzovski@gmail.com>
2014-05-01 16:26:15 +0300

    Basic login and logout

--- src/main/resources/static/iDeloR&I/js/controllers.js
+++ src/main/resources/static/iDeloR&I/js/controllers.js
@@ -61,6 +61,7 @@ iDeloControllers.controller('SearchCtrl', [ '$scope', '$routeParams', '$http',
 iDeloControllers.controller('LoginCtrl', ['$scope', '$location', 'Auth', function($scope,$location, Auth) {
         $scope.showUserMenu = true;
         $scope.showAdminMenu = false;
+        $scope.userName = getUserName();
 
 
         $scope.doAdminLogin = function() {
@@ -68,5 +69,29 @@ iDeloControllers.controller('LoginCtrl', ['$scope', '$location', 'Auth', functio
             $scope.showAdminMenu = true;
             $location.path("/");
         };
+
+        $scope.doSignIn = function(username) {
+            document.cookie="username="+username;
+        };
+
+        $scope.logout = function(username) {
+            document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        };
+
+        function getUserName() {
+            return getCookie("username")
+        };
+
+        function getCookie(cname)
+        {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++)
+            {
+                var c = ca[i].trim();
+                if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+            }
+            return "";
+        }
 } ]);
 
