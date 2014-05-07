235a4335feb1efa9a43c9f4b39d530e10034d80a
ilja <ilja.guzovski@gmail.com>
2014-05-07 21:21:07 +0300

    basic registration  (still there might be some bad cases on backend)

--- src/main/java/idelo/controller/CitizenController.java
+++ src/main/java/idelo/controller/CitizenController.java
@@ -4,6 +4,7 @@ package idelo.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import idelo.model.citzen.Citizen;
+import idelo.model.citzen.Sex;
 import idelo.repository.CitizenRepository;
 import idelo.response.JSONListBuilder;
 import idelo.response.parts.CitizenResponse;
@@ -12,7 +13,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 @Controller
@@ -45,6 +51,28 @@ public class CitizenController {
             e.printStackTrace();
         }
         return 0l;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public String register(@RequestParam("email") String email, @RequestParam("password") String password,
+                         @RequestParam("name") String name,  @RequestParam("sex") String sex,
+                         @RequestParam("date") String date,  @RequestParam("address") String address) throws ParseException {
+        Sex parsedSexOfCitizen;
+        if(sex.equalsIgnoreCase(Sex.MALE.toString())){
+            parsedSexOfCitizen = Sex.MALE;
+        }else {
+            parsedSexOfCitizen = Sex.FEMALE;
+        }
+
+        Date parsedDate;
+        parsedDate =  new SimpleDateFormat("yyyy-mm-dd", Locale.ENGLISH).parse(date);
+
+
+        Citizen newCitizen = new Citizen(name, parsedSexOfCitizen,parsedDate,address,123l,email,password);
+        System.err.println("new c " +newCitizen);
+        citizenRepository.save(newCitizen);
+        return "OK";
     }
 
 }
